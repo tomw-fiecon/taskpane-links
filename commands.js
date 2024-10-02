8577f0b1852940052f83c8ba0002af3131a10dd9
Tom Wright <tom.wright@fiecon.com>
2024-10-02 10:52:20 +0100

    JS updates

--- commands.js
+++ commands.js
@@ -1,5 +1,16 @@
+Office.onReady(() => {
+    Office.actions.associate("openCria", openCria);
+    Office.actions.associate("findAbbrevs", findAbbrevs);
+});
 
-function sayHello(event) {
+
+export function openCria(event) {
+    window.open('https://cria.fiecon.com/', '_blank');
+    event.completed();
+}
+
+
+function findAbbrevs(event) {
     Office.context.document.setSelectedDataAsync(
         "Hello World!",
         function (asyncResult) {
@@ -9,37 +20,4 @@ function sayHello(event) {
         }
     );
     event.completed();
-}
-
-Office.onReady(() => {
-    Office.actions.associate("openCria", openCria);
-    Office.actions.associate("findAbbrevs", findAbbrevs);
-});
-
-
-function openCria() {
-    window.open('https://cria.fiecon.com/', '_blank');
-}
-
-
-// function findAbbrevs(){
-    
-// }
-
-
-// Gets some details about the current slide and displays them in a notification.
-function findAbbrevs() {
-    if (Office.context.document.getSelectedDataAsync) {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('Some slide details are:', '"' + JSON.stringify(result.value) + '"');
-                } else {
-                    showNotification('Error:', result.error.message);
-                }
-            }
-        );
-    } else {
-        app.showNotification('Error:', 'Reading selection data is not supported by this host application.');
-    }
 }
