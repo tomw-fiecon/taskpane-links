edf164a8bbc21612684f99fec00e51bf2be1beac
Tom Wright <tom.wright@fiecon.com>
2024-10-03 17:24:30 +0100

    JS-ify button

--- commands.js
+++ commands.js
@@ -1,30 +1,37 @@
+/* eslint-disable no-undef */
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
 Office.onReady(() => {
+    // Register the function with Office.
     Office.actions.associate("openCria", openCria);
     Office.actions.associate("findAbbrevs", findAbbrevs);
-});
-
-
-function openCria(event) {
-    window.open('https://cria.fiecon.com/', '_blank');
+  });
+  
+  function openCria(event) {
+    window.open("https://cria.fiecon.com/", "_blank");
     event.completed();
-}
-
-
-function helloWorld(event) {
-    Office.context.document.setSelectedDataAsync(
-        "Hello World!",
-        function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                console.error(asyncResult.error.message);
-            }
-        }
-    );
+  }
+  
+  function findAbbrevs(event) {
+      Office.context.document.setSelectedDataAsync("Coming soon!", function (asyncResult) {
+          if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+            console.error(asyncResult.error.message);
+          }
+        });
+        event.completed();
+  }
+  
+  
+  
+  function helloWorld(event) {
+    Office.context.document.setSelectedDataAsync("Hello World this is a test!", function (asyncResult) {
+      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+        console.error(asyncResult.error.message);
+      }
+    });
     event.completed();
-}
-
-
-
-
-function findAbbrevs(event) {
-    event.completed();
-}
+  }
+  
